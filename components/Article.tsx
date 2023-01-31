76cf263f868059fd4dca7a8520a655f7c9f64e0b
Captain Price <murathoncu@gmail.com>
2023-01-31 20:15:10 +0300

    csr

--- components/Article.tsx
+++ components/Article.tsx
@@ -21,7 +21,7 @@ const Article = ({ article }: Props) => {
         <div className="flex-1 flex flex-col p-5">
           <h2 className="font-bold font-serif">{article.title}</h2>
           <section className="mt-2 flex-1">
-            <p className="text-xs line-clamp-6">{article.description}</p>
+            <p className="text-xs line-clamp-6">{article.snippet}</p>
           </section>
 
           <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-400">
